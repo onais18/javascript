e06f26e64b331e9b04b8d1266102f0db71758628
Mohd Onais <99065615+onais18@users.noreply.github.com>
2024-09-06 14:30:50 +0000

    devconatiner

--- .devcontainer/basic/text.js
+++ .devcontainer/basic/text.js
@@ -3,3 +3,18 @@ let acountemail="oyankler@mail.com "
 let accountpassword="234566"
 
 console.table([accountid,acountemail,accountpassword])
+
+//type conversion of data types
+let value="77"
+let intonumber=Number (value)
+console.log(typeof(intonumber))
+console.log(intonumber)
+
+console.log("2"<7)
+console.log("2"===2)
+
+// There are two types of data in javscript. 
+// Primitive datatypes: string,number,boolean,symbol,null and undifiend.
+// they store in "stack" memory due to (creating a copy nature).
+// non-primitive type: array,object and function it is also called (refrence type).
+// they store in "Heap" memory due to (chage in original value not creating a copy).
